76588d68b624ca5e36cfe9ad92d84183f395b6bb
Rodrygo010 <melgaddouch00@gmail.com>
2024-12-25 09:41:24 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Controle1Master2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Controle1Master2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Controle1Master2Application.class, args);
+	}
+
+}
